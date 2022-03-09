0fb60f40f3674f41fa1c53d5ad73e0be67df3cbc
Karsten Reincke [@perceval] <k.reincke@fodina.de>
2022-03-09 23:13:48 +0100

    Add a project collector

---
+++ bin/collprj.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+DE="de"
+EN="en"
+LANG=${DE}
+
+function mhelp {
+ echo "collprj.sh [en|de] PRJ";
+ exit 0;
+}
+
+if [ "$2" == "" ]; then mhelp; fi;
+
+PID=$2
+
+if [ "$1" == "${EN}" ] ||[ "$1" == "EN" ] ; then
+  LANG="${EN}";
+elif [ "$1" == "${DE}" ] ||[ "$1" == "DE" ] ; then
+  LANG="${DE}";
+else
+  mhelp;
+fi
+
+echo "collecting files for project $PID in language $LANG"
+
+if [ -d ${PID} ]; then rm -rf ${PID}; fi
+
+mkdir -p ${PID}
+
+cp -rd README.md source tools ${PID}
+if [ "${LANG}" == "${EN}" ] ; then
+  find ${PID} -name "*-de.tex" | while read f; do rm $f; done
+  cp sys/Makefile.en ${PID}/Makefile
+else
+  find ${PID} -name "*-en.tex" | while read f; do rm $f; done
+  cp sys/Makefile.de ${PID}/Makefile
+fi;
+
+exit 0;
